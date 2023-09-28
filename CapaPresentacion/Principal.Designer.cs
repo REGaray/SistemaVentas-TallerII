3ea95fee7985b88b979b0c46eee39cee37a498e9
REGaray <rubenernestogaray@gmail.com>
2023-09-28 15:03:59 -0300

    inicio carga de botones pantalla principal

--- CapaPresentacion/Principal.Designer.cs
+++ CapaPresentacion/Principal.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            btnUsuarios = new FontAwesome.Sharp.IconButton();
+            btnConfiguracion = new FontAwesome.Sharp.IconButton();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(1, 13, 44);
+            panel1.Controls.Add(iconButton1);
+            panel1.Controls.Add(btnConfiguracion);
+            panel1.Controls.Add(btnUsuarios);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(230, 600);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(1, 13, 44);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(230, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(720, 34);
+            panel2.TabIndex = 1;
+            // 
+            // btnUsuarios
+            // 
+            btnUsuarios.AutoEllipsis = true;
+            btnUsuarios.BackColor = Color.FromArgb(15, 49, 120);
+            btnUsuarios.FlatAppearance.BorderSize = 0;
+            btnUsuarios.FlatStyle = FlatStyle.Flat;
+            btnUsuarios.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnUsuarios.ForeColor = Color.White;
+            btnUsuarios.IconChar = FontAwesome.Sharp.IconChar.Users;
+            btnUsuarios.IconColor = Color.White;
+            btnUsuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnUsuarios.IconSize = 30;
+            btnUsuarios.ImageAlign = ContentAlignment.MiddleLeft;
+            btnUsuarios.Location = new Point(12, 115);
+            btnUsuarios.Name = "btnUsuarios";
+            btnUsuarios.Padding = new Padding(10, 0, 0, 0);
+            btnUsuarios.Size = new Size(203, 47);
+            btnUsuarios.TabIndex = 2;
+            btnUsuarios.Text = "Usuarios";
+            btnUsuarios.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnUsuarios.UseVisualStyleBackColor = false;
+            // 
+            // btnConfiguracion
+            // 
+            btnConfiguracion.AutoEllipsis = true;
+            btnConfiguracion.BackColor = Color.FromArgb(15, 49, 120);
+            btnConfiguracion.FlatAppearance.BorderSize = 0;
+            btnConfiguracion.FlatStyle = FlatStyle.Flat;
+            btnConfiguracion.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnConfiguracion.ForeColor = Color.White;
+            btnConfiguracion.IconChar = FontAwesome.Sharp.IconChar.Gears;
+            btnConfiguracion.IconColor = Color.White;
+            btnConfiguracion.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnConfiguracion.IconSize = 30;
+            btnConfiguracion.ImageAlign = ContentAlignment.MiddleLeft;
+            btnConfiguracion.Location = new Point(12, 168);
+            btnConfiguracion.Name = "btnConfiguracion";
+            btnConfiguracion.Padding = new Padding(10, 0, 0, 0);
+            btnConfiguracion.Size = new Size(203, 47);
+            btnConfiguracion.TabIndex = 3;
+            btnConfiguracion.Text = "Configuración";
+            btnConfiguracion.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnConfiguracion.UseVisualStyleBackColor = false;
+            // 
+            // iconButton1
+            // 
+            iconButton1.AutoEllipsis = true;
+            iconButton1.BackColor = Color.FromArgb(15, 49, 120);
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            iconButton1.ForeColor = Color.White;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.Gears;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 30;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(12, 221);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Padding = new Padding(10, 0, 0, 0);
+            iconButton1.Size = new Size(203, 47);
+            iconButton1.TabIndex = 4;
+            iconButton1.Text = "Configuración";
+            iconButton1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(220, 234, 252);
+            ClientSize = new Size(950, 600);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Principal";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private FontAwesome.Sharp.IconButton btnUsuarios;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnConfiguracion;
     }
 }
