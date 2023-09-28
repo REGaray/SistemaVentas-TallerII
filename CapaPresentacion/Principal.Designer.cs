2796cab12f2ad30e22eb6682484a2e9ea23e208f
REGaray <rubenernestogaray@gmail.com>
2023-09-28 18:02:14 -0300

    listos los botones de pantalla principal

--- CapaPresentacion/Principal.Designer.cs
+++ CapaPresentacion/Principal.Designer.cs
@@ -32,14 +32,22 @@
             panel2 = new Panel();
             btnUsuarios = new FontAwesome.Sharp.IconButton();
             btnConfiguracion = new FontAwesome.Sharp.IconButton();
-            iconButton1 = new FontAwesome.Sharp.IconButton();
+            btnVentas = new FontAwesome.Sharp.IconButton();
+            btnCompras = new FontAwesome.Sharp.IconButton();
+            btnClientes = new FontAwesome.Sharp.IconButton();
+            btnProveedores = new FontAwesome.Sharp.IconButton();
+            btnReportes = new FontAwesome.Sharp.IconButton();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(1, 13, 44);
-            panel1.Controls.Add(iconButton1);
+            panel1.Controls.Add(btnReportes);
+            panel1.Controls.Add(btnProveedores);
+            panel1.Controls.Add(btnClientes);
+            panel1.Controls.Add(btnCompras);
+            panel1.Controls.Add(btnVentas);
             panel1.Controls.Add(btnConfiguracion);
             panel1.Controls.Add(btnUsuarios);
             panel1.Dock = DockStyle.Left;
@@ -101,27 +109,120 @@
             btnConfiguracion.TextImageRelation = TextImageRelation.ImageBeforeText;
             btnConfiguracion.UseVisualStyleBackColor = false;
             // 
-            // iconButton1
+            // btnVentas
             // 
-            iconButton1.AutoEllipsis = true;
-            iconButton1.BackColor = Color.FromArgb(15, 49, 120);
-            iconButton1.FlatAppearance.BorderSize = 0;
-            iconButton1.FlatStyle = FlatStyle.Flat;
-            iconButton1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            iconButton1.ForeColor = Color.White;
-            iconButton1.IconChar = FontAwesome.Sharp.IconChar.Gears;
-            iconButton1.IconColor = Color.White;
-            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconButton1.IconSize = 30;
-            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
-            iconButton1.Location = new Point(12, 221);
-            iconButton1.Name = "iconButton1";
-            iconButton1.Padding = new Padding(10, 0, 0, 0);
-            iconButton1.Size = new Size(203, 47);
-            iconButton1.TabIndex = 4;
-            iconButton1.Text = "Configuración";
-            iconButton1.TextImageRelation = TextImageRelation.ImageBeforeText;
-            iconButton1.UseVisualStyleBackColor = false;
+            btnVentas.AutoEllipsis = true;
+            btnVentas.BackColor = Color.FromArgb(15, 49, 120);
+            btnVentas.FlatAppearance.BorderSize = 0;
+            btnVentas.FlatStyle = FlatStyle.Flat;
+            btnVentas.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnVentas.ForeColor = Color.White;
+            btnVentas.IconChar = FontAwesome.Sharp.IconChar.CartArrowDown;
+            btnVentas.IconColor = Color.White;
+            btnVentas.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnVentas.IconSize = 30;
+            btnVentas.ImageAlign = ContentAlignment.MiddleLeft;
+            btnVentas.Location = new Point(12, 221);
+            btnVentas.Name = "btnVentas";
+            btnVentas.Padding = new Padding(10, 0, 0, 0);
+            btnVentas.Size = new Size(203, 47);
+            btnVentas.TabIndex = 4;
+            btnVentas.Text = "Ventas";
+            btnVentas.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnVentas.UseMnemonic = false;
+            btnVentas.UseVisualStyleBackColor = false;
+            // 
+            // btnCompras
+            // 
+            btnCompras.AutoEllipsis = true;
+            btnCompras.BackColor = Color.FromArgb(15, 49, 120);
+            btnCompras.FlatAppearance.BorderSize = 0;
+            btnCompras.FlatStyle = FlatStyle.Flat;
+            btnCompras.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCompras.ForeColor = Color.White;
+            btnCompras.IconChar = FontAwesome.Sharp.IconChar.Dolly;
+            btnCompras.IconColor = Color.White;
+            btnCompras.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnCompras.IconSize = 30;
+            btnCompras.ImageAlign = ContentAlignment.MiddleLeft;
+            btnCompras.Location = new Point(12, 274);
+            btnCompras.Name = "btnCompras";
+            btnCompras.Padding = new Padding(10, 0, 0, 0);
+            btnCompras.Size = new Size(203, 47);
+            btnCompras.TabIndex = 5;
+            btnCompras.Text = "Compras";
+            btnCompras.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnCompras.UseMnemonic = false;
+            btnCompras.UseVisualStyleBackColor = false;
+            // 
+            // btnClientes
+            // 
+            btnClientes.AutoEllipsis = true;
+            btnClientes.BackColor = Color.FromArgb(15, 49, 120);
+            btnClientes.FlatAppearance.BorderSize = 0;
+            btnClientes.FlatStyle = FlatStyle.Flat;
+            btnClientes.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnClientes.ForeColor = Color.White;
+            btnClientes.IconChar = FontAwesome.Sharp.IconChar.UserGroup;
+            btnClientes.IconColor = Color.White;
+            btnClientes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnClientes.IconSize = 30;
+            btnClientes.ImageAlign = ContentAlignment.MiddleLeft;
+            btnClientes.Location = new Point(12, 327);
+            btnClientes.Name = "btnClientes";
+            btnClientes.Padding = new Padding(10, 0, 0, 0);
+            btnClientes.Size = new Size(203, 47);
+            btnClientes.TabIndex = 6;
+            btnClientes.Text = "Clientes";
+            btnClientes.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnClientes.UseMnemonic = false;
+            btnClientes.UseVisualStyleBackColor = false;
+            // 
+            // btnProveedores
+            // 
+            btnProveedores.AutoEllipsis = true;
+            btnProveedores.BackColor = Color.FromArgb(15, 49, 120);
+            btnProveedores.FlatAppearance.BorderSize = 0;
+            btnProveedores.FlatStyle = FlatStyle.Flat;
+            btnProveedores.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnProveedores.ForeColor = Color.White;
+            btnProveedores.IconChar = FontAwesome.Sharp.IconChar.TruckRampBox;
+            btnProveedores.IconColor = Color.White;
+            btnProveedores.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnProveedores.IconSize = 30;
+            btnProveedores.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProveedores.Location = new Point(12, 380);
+            btnProveedores.Name = "btnProveedores";
+            btnProveedores.Padding = new Padding(10, 0, 0, 0);
+            btnProveedores.Size = new Size(203, 47);
+            btnProveedores.TabIndex = 7;
+            btnProveedores.Text = "Proveedores";
+            btnProveedores.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProveedores.UseMnemonic = false;
+            btnProveedores.UseVisualStyleBackColor = false;
+            // 
+            // btnReportes
+            // 
+            btnReportes.AutoEllipsis = true;
+            btnReportes.BackColor = Color.FromArgb(15, 49, 120);
+            btnReportes.FlatAppearance.BorderSize = 0;
+            btnReportes.FlatStyle = FlatStyle.Flat;
+            btnReportes.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnReportes.ForeColor = Color.White;
+            btnReportes.IconChar = FontAwesome.Sharp.IconChar.ChartColumn;
+            btnReportes.IconColor = Color.White;
+            btnReportes.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnReportes.IconSize = 30;
+            btnReportes.ImageAlign = ContentAlignment.MiddleLeft;
+            btnReportes.Location = new Point(12, 433);
+            btnReportes.Name = "btnReportes";
+            btnReportes.Padding = new Padding(10, 0, 0, 0);
+            btnReportes.Size = new Size(203, 47);
+            btnReportes.TabIndex = 8;
+            btnReportes.Text = "Reportes";
+            btnReportes.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnReportes.UseMnemonic = false;
+            btnReportes.UseVisualStyleBackColor = false;
             // 
             // Principal
             // 
@@ -143,7 +244,11 @@
         private Panel panel1;
         private Panel panel2;
         private FontAwesome.Sharp.IconButton btnUsuarios;
-        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton btnVentas;
         private FontAwesome.Sharp.IconButton btnConfiguracion;
+        private FontAwesome.Sharp.IconButton btnReportes;
+        private FontAwesome.Sharp.IconButton btnProveedores;
+        private FontAwesome.Sharp.IconButton btnClientes;
+        private FontAwesome.Sharp.IconButton btnCompras;
     }
 }
