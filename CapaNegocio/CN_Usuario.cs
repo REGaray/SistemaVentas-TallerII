41d0bcd9bd6c37e3759c914a16aeff3b3a2a7dd4
Goytia Jeremias <jeremiasjg99@gmail.com>
2023-10-02 11:37:41 -0300

    Se llama a los metodos [Registrar, Editar, Eliminar] de CD_Usuarios desde CN_Usuarios

--- CapaNegocio/CN_Usuario.cs
+++ CapaNegocio/CN_Usuario.cs
@@ -23,27 +23,68 @@ namespace CapaNegocio
         {
             mensaje = string.Empty;
 
+            // Verifica si el campo Documento está vacío y agrega un mensaje de error si es necesario.
             if (obj.Documento == "")
             {
                 mensaje += "Es necesario el Documento del usuario\n";
             }
+
+            // Verifica si el campo NombreCompleto está vacío y agrega un mensaje de error si es necesario.
             if (obj.NombreCompleto == "")
             {
                 mensaje += "Es necesario el NombreCompleto del usuario\n";
             }
+
+            // Verifica si el campo Clave está vacío y agrega un mensaje de error si es necesario.
             if (obj.Clave == "")
             {
                 mensaje += "Es necesario el Clave del usuario\n";
             }
 
-            // Llama a la función "registrar" del objeto "objcd_usuario" para realizar el registro.
-            return objcd_usuario.registrar(obj, out mensaje);
+            // Si se encontraron errores de validación, devuelve 0 y establece el mensaje de error.
+            if (mensaje != string.Empty)
+            {
+                return 0;
+            }
+            else
+            {
+                // Llama a la función "registrar" del objeto "objcd_usuario" para realizar el registro.
+                return objcd_usuario.registrar(obj, out mensaje);
+            }
         }
 
         public bool editar(Usuario obj, out string mensaje)
         {
-            // Llama a la función "editar" del objeto "objcd_usuario" para realizar la edición.
-            return objcd_usuario.editar(obj, out mensaje);
+            mensaje = string.Empty;
+
+            // Verifica si el campo Documento está vacío y agrega un mensaje de error si es necesario.
+            if (obj.Documento == "")
+            {
+                mensaje += "Es necesario el Documento del usuario\n";
+            }
+
+            // Verifica si el campo NombreCompleto está vacío y agrega un mensaje de error si es necesario.
+            if (obj.NombreCompleto == "")
+            {
+                mensaje += "Es necesario el NombreCompleto del usuario\n";
+            }
+
+            // Verifica si el campo Clave está vacío y agrega un mensaje de error si es necesario.
+            if (obj.Clave == "")
+            {
+                mensaje += "Es necesario el Clave del usuario\n";
+            }
+
+            // Si se encontraron errores de validación, devuelve False y establece el mensaje de error.
+            if (mensaje != string.Empty)
+            {
+                return false;
+            }
+            else
+            {
+                // Llama a la función "editar" del objeto "objcd_usuario" para realizar la edición.
+                return objcd_usuario.editar(obj, out mensaje);
+            }
         }
 
         public bool eliminar(Usuario obj, out string mensaje)
